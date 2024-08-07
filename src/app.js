c8d55cd0fdb5e3dc1353a529bce6a1b821e589eb
Nirav0804 <niravpatel0804@gmail.com>
2024-08-07 21:04:05 +0530

    add register controller and bug fixes

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
-import userRouter from "./routes/user.js"
+import userRouter from "./routes/user.routes.js"
 
 app.use("/api/v1/users", userRouter);
 export { app }
--- src/routes/user.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { registerUsesr } from "../controllers/user.js";
-
-const router = Router();
-
-router.route("/register").post(registerUsesr)
-
-export default router;
